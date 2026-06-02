ef154b949a537018bf7619d40c423b1897b765cb
SakshamRijal1 <sakshamrijal07@gmail.com>
2026-06-02 16:02:53 +0545

    Update server.js

--- server5/server.js
+++ server5/server.js
@@ -4,13 +4,13 @@ import {inngest,functions} from "./inngest/index.js"
 import {serve} from "inngest/express"
 import cors from 'cors'
 import 'dotenv/config'
-await connectDB();
+ connectDB();
 const app=express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/inngest",serve({client:inngest,functions}));
 app.get('/',(req,res)=>{
-  res.send("Servre is running");
+  res.send("Server is running");
 })
 app.listen(process.env.PORT||4000,()=>{
   console.log(`Server is running on port ${process.env.PORT||4000}`);
