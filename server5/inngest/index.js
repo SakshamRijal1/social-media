0a48569737c6806cfba2cc4668a5a48a9d3e6a42
SakshamRijal1 <sakshamrijal07@gmail.com>
2026-06-02 14:26:36 +0545

    commit server5

---
+++ server5/inngest/index.js
@@ -0,0 +1,74 @@
+import {Inngest} from "inngest";
+import User from "./../models/User.js"
+export const inngest=new Inngest({
+  id:"SakshaMedia"
+})
+
+const syncUserCreation=inngest.createFunction(
+  {
+    id:"sync-user-creation",
+    triggers:[
+      {
+      event:"clerk/user.created"//source/object.action
+      }
+    ]
+  },
+  async((event)=>{
+    const {id,email_addresses,first_name,last_name,image_url}=event.data;
+    let username=email_addresses[0].email_address.split('@')[0];
+  const existingUser=await User.findOne(username);
+  if(existingUser)
+  {
+    username=username+Math.floor(Math.random()*1000)
+  }
+    const userCreated={
+      _id:id,
+      email:email_addresses[0].email_address,
+      full_name:first_name +" " + last_name,
+      profile_picture:image_url,
+           username=email_addresses[0].email_address.split('@')[0],
+
+    }
+    await User.create(userCreated)
+  })
+)
+const syncUserUpdation=inngest.createFunction(
+  {
+    id:"sync-user-updation",
+    triggers:[
+      {
+      event:"clerk/user.updated"//source/object.action
+      }
+    ]
+  },
+  async((event)=>{
+    const {id,email_addresses,first_name,last_name,image_url}=event.data;
+
+    const userUpdated={
+      email:email_addresses[0].email_address,
+      full_name:first_name +" " + last_name,
+      profile_picture:image_url,
+  
+
+    }
+await User.findByIdAndUpdate(id,userUpdated);
+  })
+)
+const syncUserDeletion=inngest.createFunction(
+  {
+    id:"sync-user-deletion",
+    triggers:[
+      {
+      event:"clerk/user.deleted"//source/object.action
+      }
+    ]
+  },
+  async((event)=>{
+    const {id}=event.data;
+
+await User.findByIdAndDelete(id)
+  })
+)
+
+
+export const functions=[]
