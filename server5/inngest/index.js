8134cd0b65a3d47be0119632c4745fb98caaac36
SakshamRijal1 <sakshamrijal07@gmail.com>
2026-06-02 16:09:58 +0545

    Refactor Inngest functions for user management

--- server5/inngest/index.js
+++ server5/inngest/index.js
@@ -1,76 +1,82 @@
-import {Inngest} from "inngest";
-import User from "./../models/User.js"
-export const inngest=new Inngest({
-  id:"SakshaMedia"
-})
+import { Inngest } from "inngest";
+import User from "../models/User.js";
 
-const syncUserCreation=inngest.createFunction(
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "SakshaMedia-app" });
+
+// Create an empty array where we'll export future Inngest functions
+
+
+//inngest function to save user data to a database
+
+const syncUserCreation=inngest.createFunction({
+id:'sync-user-from-clerk',
+triggers:[
   {
-    id:"sync-user-creation",
-    triggers:[
-      {
-      event:"clerk/user.created"//source/object.action
-      }
-    ]
-  },
-  async({event})=>{
-    const {id,email_addresses,first_name,last_name,image_url}=event.data;
-    let username=email_addresses[0].email_address.split('@')[0];
-  const existingUser=await User.findOne({
+    event:"clerk/user.created"
+  }
+]
+},
+
+
+async({event})=>{
+  const {id,first_name,last_name,email_addresses,image_url}=event.data;
+  let username=email_addresses[0].email_address.split('@')[0];
+  //check availibility of username
+  const user=await User.findOne({
     username
-  });
-  if(existingUser)
+  })
+  if(user)
   {
     username=username+Math.floor(Math.random()*1000)
   }
-    const userCreated={
-      _id:id,
-      email:email_addresses[0].email_address,
-      full_name:first_name +" " + last_name,
-      profile_picture:image_url,
-           username=email_addresses[0].email_address.split('@')[0],
+  const userData={
+    _id:id,
+    email:email_addresses[0].email_address,
+    full_name:first_name+" " +last_name,
+    profile_picture:image_url,
+    username,
+  }
+await User.create(userData)
+}
+)
 
-    }
-    await User.create(userCreated)
-  })
-
-const syncUserUpdation=inngest.createFunction(
+const syncUserUpdation=inngest.createFunction({
+id:'update-user-from-clerk',
+triggers:[
   {
-    id:"sync-user-updation",
-    triggers:[
-      {
-      event:"clerk/user.updated"//source/object.action
-      }
-    ]
-  },
-  async({event})=>{
-    const {id,email_addresses,first_name,last_name,image_url}=event.data;
+    event:"clerk/user.updated"
+  }
+]
+},
 
-    const userUpdated={
-      email:email_addresses[0].email_address,
-      full_name:first_name +" " + last_name,
-      profile_picture:image_url,
-  
 
-    }
-await User.findByIdAndUpdate(id,userUpdated);
-  })
+async({event})=>{
+  const {id,first_name,last_name,email_address,image_url}=event.data;
+const updatedUserData={
+  email:email_addresses[0].email_address,
+  full_name:first_name + ' ' + last_name,
+  profile_picture:image_url
+}
+await User.findByIdAndUpdate(id,updatedUserData)
+}
+)
 
-const syncUserDeletion=inngest.createFunction(
+const syncUserDeletion=inngest.createFunction({
+id:'delete-user-from-clerk',
+triggers:[
   {
-    id:"sync-user-deletion",
-    triggers:[
-      {
-      event:"clerk/user.deleted"//source/object.action
-      }
-    ]
-  },
-  async({event})=>{
-    const {id}=event.data;
-
-await User.findByIdAndDelete(id)
-  })
+    event:"clerk/user.deleted"
+  }
+]
+},
 
 
+async({event})=>{
+  const {id}=event.data;
+  await User.findByIdAndDelete(id
 
-export const functions=[syncUserCreation,syncUserUpdation,syncUserDeletion]
+  )
+}
+)
+export const functions = [syncUserCreation,syncUserUpdation,syncUserDeletion];
