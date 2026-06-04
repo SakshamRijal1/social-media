02f1fc46e95387e6980ff320ee2d11989fb23b9d
SakshamRijal1 <sakshamrijal07@gmail.com>
2026-06-04 23:46:14 +0545

    Update index.js

--- server5/inngest/index.js
+++ server5/inngest/index.js
@@ -1,8 +1,10 @@
 import {Inngest} from "inngest";
 import User from "../models/User.js";
+import mongoose from "mongoose";
 export const inngest=new Inngest({
   id:"Sakshamedia"
 })
+
 
 const syncUserCreation=inngest.createFunction(
   {
@@ -14,15 +16,20 @@ const syncUserCreation=inngest.createFunction(
     ]
   },
   async({event})=>{
+
+    console.log("Function triggered");
+    console.log("Mongo state:", mongoose.connection.readyState);
+
     const {id,email_addresses,first_name,last_name,image_url}=event.data;
     let username=email_addresses[0].email_address.split('@')[0];
-  let existingUser=await User.findOne({
+
+  let existingUsername=await User.findOne({
     username
   });
-  while(existingUser)
+  while(existingUsername)
   {
     username=username+Math.floor(Math.random()*1000);
-existingUser=await User.findOne({
+existingUsername=await User.findOne({
     username
   });
   }
